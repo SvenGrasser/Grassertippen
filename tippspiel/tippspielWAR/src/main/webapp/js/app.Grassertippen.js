89d1b33fe324ce642c884d9d10214cabe7f5cf51
Sven Grasser <sven.grasser@gmail.com>
2017-12-16 09:28:40 +0100

    Bugfix: show details

--- tippspiel/tippspielWAR/src/main/webapp/js/app.Grassertippen.js
+++ tippspiel/tippspielWAR/src/main/webapp/js/app.Grassertippen.js
@@ -44,8 +44,7 @@ function load($scope, $http) {
 function showMemberSummaryDetails($scope, $http, memberSummary) {
 	if(memberSummary.memberDetails == null) {	
 		$http.get("/tippspiel/api/v1/members/" + memberSummary.member.id + "?"  + Math.random())
-		.then(function(response) {
-			$scope.memberDetails = response.data;		
+		.then(function(response) {	
 			$scope.detailsHidden = false;
 			memberSummary.memberDetails = response.data;
 		});
