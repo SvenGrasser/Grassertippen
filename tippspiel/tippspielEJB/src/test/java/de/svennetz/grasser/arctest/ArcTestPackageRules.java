b7aa17e563c6d9d3cd084440aba46ab69ccf56b6
Sven Grasser <sven.grasser@web.de>
2018-06-21 20:19:54 +0200

    exclude test classes from ArchUnit

--- tippspiel/tippspielEJB/src/test/java/de/svennetz/grasser/arctest/ArcTestPackageRules.java
+++ tippspiel/tippspielEJB/src/test/java/de/svennetz/grasser/arctest/ArcTestPackageRules.java
@@ -2,16 +2,14 @@ package de.svennetz.grasser.arctest;
 
 import org.junit.runner.RunWith;
 
+import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchRules;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.junit.ArchUnitRunner;
-import com.tngtech.archunit.lang.ArchRule;
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 
 @RunWith(ArchUnitRunner.class)
-@AnalyzeClasses(packages = "de.svennetz.grasser.tippspiel")
+@AnalyzeClasses(packages = "de.svennetz.grasser.tippspiel", importOptions = ImportOption.DontIncludeTests.class)
 public class ArcTestPackageRules {
 	@ArchTest
     public static final ArchRules serviceRules = ArchRules.in(ServiceRules.class);
